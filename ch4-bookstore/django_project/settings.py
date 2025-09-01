ecf5d2d2e5b00c9f13f217dc13e053f685385b61
joshdels <deleonjoshy2@gmail.com>
2025-09-01 12:05:20 +0800

    added new ip address 3

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY=env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
 
-ALLOWED_HOSTS = ['149.28.129.119', "localhost", "127.0.0.1",]
+ALLOWED_HOSTS = ['149.28.129.119', "localhost", "127.0.0.1", '*']
 
 
 # Application definition
